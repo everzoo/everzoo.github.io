05fcceb2974f02c6df2b4c9cee202ff790de82dd
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2025-03-21 14:16:45 +0900

    remove placeholder, add mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import App from './App'
+
+import '@mantine/core/styles.css'
 import './index.css'
-import App from './App.tsx'
+import { MantineProvider } from '@mantine/core'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MantineProvider defaultColorScheme="light">
+      <App />
+    </MantineProvider>
   </StrictMode>,
 )
